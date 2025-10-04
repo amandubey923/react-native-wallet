90c2e2004dbb7a0bc343b57668a376e912e86864
amandubey923 <kumaraman19137@gmail.com>
2025-10-04 14:30:55 +0530

    basic setup

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import {neon} from '@neondatabase/serverless';
+import "dotenv/config";
+
+//create a sql connection using our db url
+export const sql = neon(process.env.DATABASE_URL);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {sql} from './config/db.js';
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT
 
+async function initDB(){
+    try {
+        await sql
+    } catch (error) {
+        
+    }
+}
 
 app.get('/', (req, res) =>{
     res.send("its working");
