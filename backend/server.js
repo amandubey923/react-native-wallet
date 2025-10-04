b634cbcaa6c48ec00f76ddcca76a58517b7530d6
amandubey923 <kumaraman19137@gmail.com>
2025-10-04 15:37:25 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,18 @@ const PORT = process.env.PORT
 
 async function initDB(){
     try {
-        await sql
+        await sql`CREATE TABLE IF NOT EXISTS  transactions (
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10, 2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+        console.log("Table created successfully");
     } catch (error) {
-        
+        console.error("Error creating table:", error);
+        process.exit(1); //status code 1 means failure , 0 means success
     }
 }
 
@@ -21,6 +30,8 @@ app.get('/', (req, res) =>{
 console.log("MY port:",process.env.PORT) || 5002;
 
 
-app.listen(PORT, () => {
+initDB().then(() => {
+    app.listen(PORT, () => {
     console.log('Server is running on port :', PORT);
+})
 })
